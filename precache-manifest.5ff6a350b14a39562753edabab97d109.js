72d4bf0db067f55af09c28429490b44dbbcea2e9
Khrulenko Vitalii <khrulenko.v@gmail.com>
2021-01-27 02:14:05 +0200

    Updates

---
+++ precache-manifest.5ff6a350b14a39562753edabab97d109.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "436ea092cdd8c181fe9e98b712fb51f4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ac7f3e1603d587be6c1a",
+    "url": "./static/css/main.704b7000.chunk.css"
+  },
+  {
+    "revision": "c639a8bdef133239f5cd",
+    "url": "./static/js/2.99b844bb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.99b844bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac7f3e1603d587be6c1a",
+    "url": "./static/js/main.ffbf2c61.chunk.js"
+  },
+  {
+    "revision": "0c165204287428b65967",
+    "url": "./static/js/runtime-main.a37292e9.js"
+  }
+]);
